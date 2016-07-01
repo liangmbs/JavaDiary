d725df9d5953cb541134ca505bfb2d9ac5aee587
steven <liang.chis@husky.neu.edu>
2016-07-01 18:01:34 -0400

    JAVA Class Introduction

---
+++ OOP/src/com/company/Main.java
@@ -0,0 +1,16 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+
+        Car porsche = new Car();
+        Car holden = new Car();
+
+        porsche.model = "Boxter";
+        porsche.setDoors(123);
+        porsche.getDoors();
+    }
+
+}
---
+++ OOP/src/com/company/car.java
@@ -0,0 +1,21 @@
+package com.company;
+
+/**
+ * Created by steven on 7/1/16.
+ */
+public class Car {
+
+    private int doors;
+    private int wheels;
+    public String model;
+    private String colour;
+
+    public void setDoors(int doors){
+        this.doors = doors;
+    }
+
+    public int getDoors(){
+        return this.doors;
+    }
+
+}
