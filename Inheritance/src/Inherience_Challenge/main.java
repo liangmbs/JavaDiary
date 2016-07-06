9c42019ee5870c389671b5e388b1a7a05856b5ab
steven <liang.chis@husky.neu.edu>
2016-07-06 14:18:16 -0400

    add inherientance challenge

---
+++ Inheritance/src/Inherience_Challenge/main.java
@@ -0,0 +1,12 @@
+package Inherience_Challenge;
+
+/**
+ * Created by steven on 7/6/16.
+ */
+public class main extends Object{
+
+
+    public static void main(String[] args){
+
+    }
+}
---
+++ Inheritance/src/Inherience_Challenge/vehicle.java
@@ -0,0 +1,7 @@
+package Inherience_Challenge;
+
+/**
+ * Created by steven on 7/6/16.
+ */
+public class vehicle {
+}
